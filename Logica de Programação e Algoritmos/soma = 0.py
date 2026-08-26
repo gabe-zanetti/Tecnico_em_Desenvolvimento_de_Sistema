69390050c97cce606b3890f00642c7c4725384c9
GABRIELA NOGUEIRA DE CARVALHO FELIX ZANETTI <gabriela.zanetti3@portalsesisp.org.br>
2026-08-26 11:24:35 -0300

    Postando atividades de banco de dados no dia 28 de agosto de 2026 as 11:25

---
+++ Logica de Programação e Algoritmos/soma = 0.py
@@ -0,0 +1,6 @@
+soma = 0 
+for i in range(3): 
+ numero = int(input("Digite um número: ")) 
+soma = soma + numero 
+print(soma)
+
---
+++ Logica de Programação e Algoritmos/tentativalua.lua
@@ -0,0 +1,13 @@
+print("Tentativa lua")
+
+minha_tabela = { ---O que é uma tabela??
+  x = 10,
+  y = 20
+}
+
+print( minha_tabela.x )
+print( minha_tabela.y )
+
+tabela = {} ---Parece um tipo de matriz ou array
+tabela[1] = "Lua"
+print( tabela[1])
